d10fcdd7deec588a4f4f5ed17dc9f7dfd16e1f00
Ivanou <u.ivanou@godeltech.com>
2019-10-28 21:33:26 +0300

    table design

--- client/src/components/scrollMenu.jsx
+++ client/src/components/scrollMenu.jsx
@@ -66,13 +66,13 @@ export default class ScrollGames extends React.Component {
             const teamTwo = this.state.teams.find(team => team.teamName === game.team2);
             const imgTeam2 = teamTwo ? teamTwo.image : defaultImage;
             return <div className="fadeIn" key={game.id}>
-                <div key={game.id+1} className="team"><img className="avatar" src={imgTeam1} alt=""/></div>
                 <div key={game.id+2} className="team">{game.team1}</div>
+                <div key={game.id+1} className="team"><img className="avatar" src={imgTeam1} alt=""/></div>
                 <div key={game.id+3} className={game.approved ? "score" : "unapproved"}>{game.score1} </div>
                 <div key={game.id+4} className="score"> : </div>
                 <div key={game.id+5} className={game.approved ? "score" : "unapproved"}> {game.score2}</div>
-                <div key={game.id+6} className="team">{game.team2}</div>
                 <div key={game.id+7} className="team"><img className="avatar" src={imgTeam2} alt=""/></div>
+                <div key={game.id+6} className="team">{game.team2}</div>
             </div>
         });
 
